28c11ba27eb5fd0dafa8517e254868e0e536697e
ih8unem <ih8unem@gmail.com>
2018-09-06 22:41:03 +0300

    алгоритмы поиска

--- probability/src/likelyHoodWeighting.kt
+++ probability/src/likelyHoodWeighting.kt
@@ -5,19 +5,19 @@ fun ask(x: Node, vararg dataSet: Pair<Node, Boolean>): List<Double> {
 fun likelyHoodWeighting(x: Node, root: Node, dataSet: Set<Node>, n: Int): List<Double> {
     val xW = mutableMapOf(true to 0.0, false to 0.0)
     for (i in 1..n) {
-        val w = randomSelect(root, dataSet, 1.0)
+        val w = weightedSample(root, dataSet, 1.0)
         xW[x.value] = xW[x.value]!! + w
     }
     return xW.values.toList().normalize()
 }
 
-private fun randomSelect(node: Node, dataSet: Set<Node>, weight: Double): Double {
+private fun weightedSample(node: Node, e: Set<Node>, weight: Double): Double {
     var w = weight
-    if (node !in dataSet) node.randomValue()
+    if (node !in e) node.randomValue()
     else w *= node.p()
     node.children.forEach {
         if (it.parents.size == 1 || it.parents.indexOf(node) == it.parents.size-1)
-            w = randomSelect(it, dataSet, w)
+            w = weightedSample(it, e, w)
     }
     return w
 }
